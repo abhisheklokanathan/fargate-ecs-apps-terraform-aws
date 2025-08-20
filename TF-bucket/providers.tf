c23abd80c85e4a182f7bedd0ac4accaec3719b3f
abhisheklokanathan <90037579+abhisheklokanathan@users.noreply.github.com>
2025-08-20 13:56:15 +0530

    Update providers.tf

--- TF-bucket/providers.tf
+++ TF-bucket/providers.tf
@@ -8,6 +8,16 @@ terraform {
   required_version = ">= 1.2.0"
 }
 
+  backend "s3" {
+    bucket = "databucketfortfs3"
+    key = "demo/terrafrom.tf.state"
+    region = "ap-south-1"
+    dynamodb_table = "terraform_state"
+    encrypt = true
+    
+  }
+}
+
 # --- Providers ---
 provider "aws" {
   region = "us-east-1"
