a2cefe01418d35bf2f3edb4f9f66dc0f2f158ab4
abhisheklokanathan <90037579+abhisheklokanathan@users.noreply.github.com>
2025-08-21 10:46:07 +0530

    Update provider.tf

--- Platform/provider.tf
+++ Platform/provider.tf
@@ -6,10 +6,10 @@ terraform {
     }
   }
   backend "s3" {
-    bucket = "databucketfortfs3"
+    bucket = "databucketfortfecs"
     key    = "demo/terraform.tf.state"
-    region = "ap-south-1"
-    dynamodb_table = "terraform_state"
+    region = "us-east-1"
+    dynamodb_table = "terraform_state_ecs"
     encrypt = true
   }
 }
@@ -17,5 +17,5 @@ terraform {
 #Configure the AWS Provider
 provider "aws" {
   alias = "south"
-  region = "ap-south-1"
+  region = "ap-east-1"
 }
